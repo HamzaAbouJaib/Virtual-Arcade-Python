4e0b69c3533578f1e955f4239b3aef3d645f069a
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-02-26 15:13:02 -0500

    Create methods to detect collisions between snake and wall or snake and itself

--- snake/snake_game.py
+++ snake/snake_game.py
@@ -24,3 +24,30 @@ class SnakeGame():
             # Slow down the update speed
             sleep(0.1)
             self.snake.move()
+
+            # Detect collisions with the wall/boarder
+            game_on = not self.wall_collision()
+
+            # Detect collisions with self
+            game_on = not self.self_collision()
+
+
+
+    def wall_collision(self):
+        """
+        If the snake's head is outside of the screen, return True
+        :return: The x and y coordinates of the snake's head.
+        """
+        return self.snake.head.xcor() > 580 or self.snake.head.xcor() < -580 or self.snake.head.ycor() > 580 or self.snake.head.ycor() < -580
+    
+
+    def self_collision(self):
+        """
+        If the distance between the head and any other segment is less than 10 it means the segments likely touched, return True. Otherwise,
+        return False
+        :return: The distance between the head and the segment.
+        """
+        for segment in self.snake.segments[1:]:
+            if self.snake.head.distance(segment) < 10:
+                return True
+        return False
