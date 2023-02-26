9772f981c61a5f4237955b490b2704f60d2f0f8a
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-02-26 14:43:29 -0500

    Create a method to extend the snake by one segment

--- snake/snake.py
+++ snake/snake.py
@@ -9,22 +9,30 @@ class Snake():
 
         # Create the initial three segments that make up the snake
         for position in self.positions:
-            segment = Turtle("square")
-            segment.color("white")
-            segment.penup()
-            segment.goto(position)
-            self.segments.append(segment)
+            self.add_segment(position)
 
         # The head of the snake
         self.head = self.segments[0]
 
 
     def add_segment(self, position):
+        """
+        The function add_segment() takes in a position and creates a new segment at that position
+        
+        :param position: The position of the new segment
+        """
         segment = Turtle("square")
         segment.color("white")
         segment.penup()
         segment.goto(position)
         self.segments.append(segment)
+
+    
+    def extend_snake(self):
+        """
+        It adds a new segment to the snake's body, positioned at the end of the snake's body
+        """
+        self.add_segment(self.segments[-1].position())
 
         
 
