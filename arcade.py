968374c808ee84d9e2c60d7a559d9f75f07a1a6c
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-02-26 14:26:48 -0500

    Change screen title and size based on game

--- arcade.py
+++ arcade.py
@@ -2,7 +2,15 @@ from turtle import Screen
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.bgcolor("black")
 
 game_choice = screen.textinput(title="Select a game", prompt="Which game would you like to play? (snake/pong): ")
+
+if game_choice.lower() == "snake":
+    screen.title("Snake Game")
+
+elif game_choice.lower() == "pong":
+    screen.title("Pong Game")
+    screen.setup(width=800, height=600)
 
 screen.exitonclick()
