da180b57b9b1b088a0f55bb01c101e1244da00b0
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-02-28 13:09:05 -0500

    Add paddles to the screen and add event listens to move the paddles

--- pong/pong_game.py
+++ pong/pong_game.py
@@ -1,4 +1,6 @@
 from turtle import Screen
+from pong.paddle import Paddle
+from time import sleep
 
 
 class PongGame():
@@ -8,6 +10,20 @@ class PongGame():
         self.screen.title("Pong Game")
         self.screen.setup(width=800, height=600)
 
+        self.right_paddle = Paddle(350, 0)
+        self.left_paddle = Paddle(-350, 0)
+
 
     def run_game(self):
-        pass
+        self.screen.listen()
+        self.screen.onkeypress(self.right_paddle.move_up, "Up")
+        self.screen.onkeypress(self.right_paddle.move_down, "Down")
+        self.screen.onkeypress(self.left_paddle.move_up, "w")
+        self.screen.onkeypress(self.left_paddle.move_down, "s")
+
+
+        while True:
+            self.screen.update()
+            sleep(0.1)
+
+
